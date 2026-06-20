c39aaae00b1f7fed1ce8d0107805c5d2812699f0
zzdonkey0327 <exonina2018027@gmail.com>
2026-06-20 16:06:30 +0800

    新增支出頁面 UI 與 SQLite 整合功能
    在 ExpensePage.Designer.cs 中新增多個 UI 元件，包括下拉選單、數字輸入框、日期選擇器、按鈕和資料表格，並調整部分標籤的文字與位置。
    
    在 ExpensePage.cs 中新增頁面載入與按鈕點擊事件邏輯，實現初始化下拉選單、從 SQLite 載入資料至 DataGridView、新增記錄至資料庫並重新載入表格，以及清空輸入區的功能。
    
    更新 TimeisMoney.csproj，移除手動引用的 SQLite DLL，改用 PackageReference 引入 Microsoft.Data.Sqlite NuGet 套件，並移除 packages.config。

--- TimeisMoney/ExpensePage.Designer.cs
+++ TimeisMoney/ExpensePage.Designer.cs
@@ -29,23 +29,170 @@
         private void InitializeComponent()
         {
             this.label1 = new System.Windows.Forms.Label();
+            this.cmbType = new System.Windows.Forms.ComboBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.cmbCategory = new System.Windows.Forms.ComboBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.numAmount = new System.Windows.Forms.NumericUpDown();
+            this.label4 = new System.Windows.Forms.Label();
+            this.dtpRecordDate = new System.Windows.Forms.DateTimePicker();
+            this.label5 = new System.Windows.Forms.Label();
+            this.cmbPaymentMethod = new System.Windows.Forms.ComboBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.txtNote = new System.Windows.Forms.TextBox();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.dgvExpenses = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.numAmount)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvExpenses)).BeginInit();
             this.SuspendLayout();
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(16, 114);
+            this.label1.Location = new System.Drawing.Point(52, 40);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(50, 18);
+            this.label1.Size = new System.Drawing.Size(85, 18);
             this.label1.TabIndex = 0;
-            this.label1.Text = "label1";
+            this.label1.Text = "收支類型:";
+            // 
+            // cmbType
+            // 
+            this.cmbType.FormattingEnabled = true;
+            this.cmbType.Location = new System.Drawing.Point(143, 37);
+            this.cmbType.Name = "cmbType";
+            this.cmbType.Size = new System.Drawing.Size(121, 26);
+            this.cmbType.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(52, 77);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(49, 18);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "分類:";
+            // 
+            // cmbCategory
+            // 
+            this.cmbCategory.FormattingEnabled = true;
+            this.cmbCategory.Location = new System.Drawing.Point(140, 77);
+            this.cmbCategory.Name = "cmbCategory";
+            this.cmbCategory.Size = new System.Drawing.Size(121, 26);
+            this.cmbCategory.TabIndex = 5;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(58, 116);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(49, 18);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "金額:";
+            // 
+            // numAmount
+            // 
+            this.numAmount.Location = new System.Drawing.Point(143, 116);
+            this.numAmount.Maximum = new decimal(new int[] {
+            9999999,
+            0,
+            0,
+            0});
+            this.numAmount.Name = "numAmount";
+            this.numAmount.Size = new System.Drawing.Size(120, 29);
+            this.numAmount.TabIndex = 7;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(58, 151);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(49, 18);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "日期:";
+            // 
+            // dtpRecordDate
+            // 
+            this.dtpRecordDate.Location = new System.Drawing.Point(140, 151);
+            this.dtpRecordDate.Name = "dtpRecordDate";
+            this.dtpRecordDate.Size = new System.Drawing.Size(200, 29);
+            this.dtpRecordDate.TabIndex = 9;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(61, 197);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(85, 18);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "付款方式:";
+            // 
+            // cmbPaymentMethod
+            // 
+            this.cmbPaymentMethod.FormattingEnabled = true;
+            this.cmbPaymentMethod.Location = new System.Drawing.Point(164, 197);
+            this.cmbPaymentMethod.Name = "cmbPaymentMethod";
+            this.cmbPaymentMethod.Size = new System.Drawing.Size(121, 26);
+            this.cmbPaymentMethod.TabIndex = 11;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(64, 246);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(49, 18);
+            this.label6.TabIndex = 12;
+            this.label6.Text = "備註:";
+            // 
+            // txtNote
+            // 
+            this.txtNote.Location = new System.Drawing.Point(140, 246);
+            this.txtNote.Name = "txtNote";
+            this.txtNote.Size = new System.Drawing.Size(100, 29);
+            this.txtNote.TabIndex = 13;
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Location = new System.Drawing.Point(382, 238);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(115, 40);
+            this.btnAdd.TabIndex = 14;
+            this.btnAdd.Text = "新增紀錄";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            // 
+            // dgvExpenses
+            // 
+            this.dgvExpenses.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dgvExpenses.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvExpenses.Location = new System.Drawing.Point(29, 281);
+            this.dgvExpenses.Name = "dgvExpenses";
+            this.dgvExpenses.RowHeadersWidth = 62;
+            this.dgvExpenses.RowTemplate.Height = 31;
+            this.dgvExpenses.Size = new System.Drawing.Size(492, 107);
+            this.dgvExpenses.TabIndex = 15;
             // 
             // ExpensePage
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 18F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.dgvExpenses);
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.txtNote);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.cmbPaymentMethod);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.dtpRecordDate);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.numAmount);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.cmbCategory);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.cmbType);
             this.Controls.Add(this.label1);
             this.Name = "ExpensePage";
+            this.Size = new System.Drawing.Size(538, 388);
+            this.Load += new System.EventHandler(this.ExpensePage_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.numAmount)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvExpenses)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -54,5 +201,18 @@
         #endregion
 
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox cmbType;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ComboBox cmbCategory;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.NumericUpDown numAmount;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.DateTimePicker dtpRecordDate;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.ComboBox cmbPaymentMethod;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox txtNote;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.DataGridView dgvExpenses;
     }
 }
--- TimeisMoney/ExpensePage.cs
+++ TimeisMoney/ExpensePage.cs
@@ -1,12 +1,8 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
 using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
+using Microsoft.Data.Sqlite;
+using TimeisMoney.Helpers; // 確保有引入 DatabaseHelper
 
 namespace TimeisMoney
 {
@@ -15,6 +11,95 @@ namespace TimeisMoney
         public ExpensePage()
         {
             InitializeComponent();
+
+            // 綁定事件
+            this.Load += ExpensePage_Load;
+            btnAdd.Click += BtnAdd_Click;
+        }
+
+        // 當頁面載入時觸發
+        private void ExpensePage_Load(object sender, EventArgs e)
+        {
+            // 初始化下拉選單選項
+            cmbType.Items.AddRange(new string[] { "支出", "收入" });
+            cmbType.SelectedIndex = 0; // 預設選取第一項
+
+            cmbCategory.Items.AddRange(new string[] { "飲食", "交通", "購物", "娛樂", "薪資", "其他" });
+            cmbCategory.SelectedIndex = 0;
+
+            cmbPaymentMethod.Items.AddRange(new string[] { "現金", "信用卡", "行動支付", "轉帳" });
+            cmbPaymentMethod.SelectedIndex = 0;
+
+            // 載入資料庫資料到 DataGridView
+            LoadData();
+        }
+
+        // 從 SQLite 讀取資料並綁定到 DataGridView
+        private void LoadData()
+        {
+            using (var connection = DatabaseHelper.GetConnection())
+            {
+                connection.Open();
+                // 撈取所有紀錄，並依日期由新到舊排序
+                string query = "SELECT * FROM ExpenseRecords ORDER BY RecordDate DESC";
+
+                using (var cmd = new SqliteCommand(query, connection))
+                {
+                    using (var reader = cmd.ExecuteReader())
+                    {
+                        DataTable dt = new DataTable();
+                        dt.Load(reader); // 將 SqliteDataReader 的資料轉入 DataTable
+                        dgvExpenses.DataSource = dt; // 綁定到畫面的表格
+                    }
+                }
+            }
+        }
+
+        // 新增按鈕點擊事件
+        private void BtnAdd_Click(object sender, EventArgs e)
+        {
+            // 簡單防呆檢查
+            if (numAmount.Value <= 0)
+            {
+                MessageBox.Show("請輸入大於 0 的金額！", "提示", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+
+            using (var connection = DatabaseHelper.GetConnection())
+            {
+                connection.Open();
+
+                // 使用參數化查詢 (@變數) 防止 SQL Injection，這是好習慣！
+                string insertQuery = @"
+                    INSERT INTO ExpenseRecords (Type, Category, Amount, RecordDate, Note, PaymentMethod)
+                    VALUES (@Type, @Category, @Amount, @RecordDate, @Note, @PaymentMethod)";
+
+                using (var cmd = new SqliteCommand(insertQuery, connection))
+                {
+                    cmd.Parameters.AddWithValue("@Type", cmbType.SelectedItem.ToString());
+                    cmd.Parameters.AddWithValue("@Category", cmbCategory.SelectedItem.ToString());
+                    cmd.Parameters.AddWithValue("@Amount", numAmount.Value);
+                    // 將日期格式化為字串儲存
+                    cmd.Parameters.AddWithValue("@RecordDate", dtpRecordDate.Value.ToString("yyyy-MM-dd"));
+                    cmd.Parameters.AddWithValue("@Note", txtNote.Text);
+                    cmd.Parameters.AddWithValue("@PaymentMethod", cmbPaymentMethod.SelectedItem.ToString());
+
+                    cmd.ExecuteNonQuery(); // 執行寫入
+                }
+            }
+
+            MessageBox.Show("新增成功！", "成功", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+            LoadData(); // 重新載入表格，讓新資料顯示出來
+            ClearInput(); // 寫一個小方法把輸入框清空
+        }
+
+        // 清空輸入區的輔助方法
+        private void ClearInput()
+        {
+            numAmount.Value = 0;
+            txtNote.Clear();
+            dtpRecordDate.Value = DateTime.Now;
         }
     }
 }
