855d1e93c5e2a45f61faf74e3950d0787e8e6f8d
zzdonkey0327 <exonina2018027@gmail.com>
2026-06-21 21:49:18 +0800

    添加logo

--- TimeisMoney/MainForm.Designer.cs
+++ TimeisMoney/MainForm.Designer.cs
@@ -28,17 +28,21 @@ namespace TimeisMoney
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
             this.panelMenu = new System.Windows.Forms.Panel();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.btnSettings = new System.Windows.Forms.Button();
             this.btnTask = new System.Windows.Forms.Button();
             this.btnExpense = new System.Windows.Forms.Button();
             this.btnDashboard = new System.Windows.Forms.Button();
             this.panelMainContent = new System.Windows.Forms.Panel();
             this.panelMenu.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
             // panelMenu
             // 
+            this.panelMenu.Controls.Add(this.pictureBox1);
             this.panelMenu.Controls.Add(this.btnSettings);
             this.panelMenu.Controls.Add(this.btnTask);
             this.panelMenu.Controls.Add(this.btnExpense);
@@ -49,12 +53,23 @@ namespace TimeisMoney
             this.panelMenu.Size = new System.Drawing.Size(200, 600);
             this.panelMenu.TabIndex = 0;
             // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.pictureBox1.Image = global::TimeisMoney.Properties.Resources.未命名設計;
+            this.pictureBox1.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(200, 200);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 4;
+            this.pictureBox1.TabStop = false;
+            // 
             // btnSettings
             // 
-            this.btnSettings.Dock = System.Windows.Forms.DockStyle.Top;
+            this.btnSettings.Dock = System.Windows.Forms.DockStyle.Bottom;
             this.btnSettings.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.btnSettings.Font = new System.Drawing.Font("微軟正黑體", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
-            this.btnSettings.Location = new System.Drawing.Point(0, 150);
+            this.btnSettings.Location = new System.Drawing.Point(0, 400);
             this.btnSettings.Name = "btnSettings";
             this.btnSettings.Size = new System.Drawing.Size(200, 50);
             this.btnSettings.TabIndex = 3;
@@ -63,10 +78,10 @@ namespace TimeisMoney
             // 
             // btnTask
             // 
-            this.btnTask.Dock = System.Windows.Forms.DockStyle.Top;
+            this.btnTask.Dock = System.Windows.Forms.DockStyle.Bottom;
             this.btnTask.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.btnTask.Font = new System.Drawing.Font("微軟正黑體", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
-            this.btnTask.Location = new System.Drawing.Point(0, 100);
+            this.btnTask.Location = new System.Drawing.Point(0, 450);
             this.btnTask.Name = "btnTask";
             this.btnTask.Size = new System.Drawing.Size(200, 50);
             this.btnTask.TabIndex = 2;
@@ -75,10 +90,10 @@ namespace TimeisMoney
             // 
             // btnExpense
             // 
-            this.btnExpense.Dock = System.Windows.Forms.DockStyle.Top;
+            this.btnExpense.Dock = System.Windows.Forms.DockStyle.Bottom;
             this.btnExpense.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.btnExpense.Font = new System.Drawing.Font("微軟正黑體", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
-            this.btnExpense.Location = new System.Drawing.Point(0, 50);
+            this.btnExpense.Location = new System.Drawing.Point(0, 500);
             this.btnExpense.Name = "btnExpense";
             this.btnExpense.Size = new System.Drawing.Size(200, 50);
             this.btnExpense.TabIndex = 1;
@@ -87,10 +102,10 @@ namespace TimeisMoney
             // 
             // btnDashboard
             // 
-            this.btnDashboard.Dock = System.Windows.Forms.DockStyle.Top;
+            this.btnDashboard.Dock = System.Windows.Forms.DockStyle.Bottom;
             this.btnDashboard.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.btnDashboard.Font = new System.Drawing.Font("微軟正黑體", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
-            this.btnDashboard.Location = new System.Drawing.Point(0, 0);
+            this.btnDashboard.Location = new System.Drawing.Point(0, 550);
             this.btnDashboard.Name = "btnDashboard";
             this.btnDashboard.Size = new System.Drawing.Size(200, 50);
             this.btnDashboard.TabIndex = 0;
@@ -112,10 +127,12 @@ namespace TimeisMoney
             this.ClientSize = new System.Drawing.Size(1000, 600);
             this.Controls.Add(this.panelMainContent);
             this.Controls.Add(this.panelMenu);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "MainForm";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Time is money個人記帳與時間成本管理系統";
             this.panelMenu.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -128,6 +145,7 @@ namespace TimeisMoney
         private System.Windows.Forms.Button btnSettings;
         private System.Windows.Forms.Button btnTask;
         private System.Windows.Forms.Panel panelMainContent;
+        private System.Windows.Forms.PictureBox pictureBox1;
     }
 }
 
